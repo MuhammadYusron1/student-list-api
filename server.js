6669514737ddf338ce9dc8c9a4007f390d457ac8
Muhammad Yusron Turmudzi <mhmmdyusront0101@gmail.com>
2024-01-07 21:20:55 +0700

    Added student route

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
+// Importing files
+const connectDB = require('./config/db');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const studentListRouter = require('./routes/studentRoute');
+
+// Connect to MongoDB
+connectDB();
+
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+app.use('/api/students', studentListRouter);
+
+// Using error handler to display error in development
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Example app is listening on port ${PORT}`)
