b996e834b5032f5d4340f2c786de09d9b799fd1a
Muhammad Yusron Turmudzi <mhmmdyusront0101@gmail.com>
2024-01-09 23:44:40 +0700

    Added basic student model

--- models/studentModel.js
+++ models/studentModel.js
@@ -2,9 +2,21 @@ import mongoose from "mongoose";
 
 const studentSchema = mongoose.Schema(
     {
-
+        name: {type: String, required: true, minLength: 2, maxLength: 36},
+        age: {type: Number, required: true, min: 15, max: 20},
+        sex: {
+            type: String, 
+            required: true, 
+            enum: {
+                values: ['M', 'F'],
+                message: '{VALUE} is not supported'
+            }
+        },
+        address: {type: String, required: true, maxLength: 64},
     },
     {
         timestamps: true,
     }
 );
+
+export default mongoose.model('Student', studentSchema);
