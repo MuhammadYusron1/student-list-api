167ad4664b509a10a6f535fe65438f5de1d2507f
Muhammad Yusron Turmudzi <mhmmdyusront0101@gmail.com>
2024-01-23 21:57:33 +0700

    Edited comment

--- routes/studentRoute.js
+++ routes/studentRoute.js
@@ -4,16 +4,16 @@ const router = express.Router();
 // Importing files from controllers
 import { getStudent, setStudent, updateStudent, deleteStudent } from '../controllers/studentController.js';
 
-//* List all students
+// List all students
 router.route("/").get(getStudent);
 
-//* Create a new student
+// Create a new student
 router.route("/set").post(setStudent);
 
-//* Find and update an existing student
+// Find and update an existing student
 router.route("/update/:id").put(updateStudent);
 
-//* Delete an existing student
+// Delete an existing student
 router.route("/delete/:id").delete(deleteStudent);
 
 export default router;
