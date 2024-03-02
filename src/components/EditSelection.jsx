3b5e9f13cd64eab5fb77fd7848cac2cd737bc015
Muhammad Yusron Turmudzi <mhmmdyusront0101@gmail.com>
2024-03-02 17:15:19 +0700

    Added state condition

--- src/components/EditSelection.jsx
+++ src/components/EditSelection.jsx
@@ -1,10 +1,12 @@
 import EditStudentForm from './EditStudentForm.jsx';
 import DeleteStudent from './DeleteStudent.jsx';
 
-export default function Selection({active, onActiveChange, editActive, onEditActiveChange, handleDelete, handleEdit, putName, setPutName, putStudentID, setPutStudentID, putAge, setPutAge, putSex, setPutSex, putAddress, setPutAddress, putSubject, setPutSubject}) {
+export default function Selection({active, onActiveChange, editActive, onEditActiveChange, stdID, student_ID, handleDelete, handleEdit, putName, setPutName, putStudentID, setPutStudentID, putAge, setPutAge, putSex, setPutSex, putAddress, setPutAddress, putSubject, setPutSubject}) {
 
 	return (
-		<div className={editActive.editSelection ? "z-50 invisible bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute top-[17px] right-auto bottom-auto left-[-156px]" : "z-50 visible bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute top-[17px] right-auto bottom-auto left-[-156px]"}>
+		<div className={(editActive.editSelection || student_ID != stdID) ? // if edit selection is true or unique ID in different from current selected ID
+										"z-50 invisible bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute top-[17px] right-auto bottom-auto left-[-156px]" : 
+										"z-50 visible bg-white divide-y divide-gray-100 rounded shadow w-44 dark:bg-gray-700 dark:divide-gray-600 absolute top-[17px] right-auto bottom-auto left-[-156px]"}>
 			<div className="py-1 text-sm">
 				<div>
 					<button onClick={() => onActiveChange({...active, edit: !active.edit})} className="flex items-center w-full px-4 py-2 text-gray-700 capitalize hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white dark:text-gray-200">
