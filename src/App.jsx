38bb50f7b48229a786f9686053afc08a65dbe9eb
Muhammad Yusron Turmudzi <mhmmdyusront0101@gmail.com>
2024-03-12 16:34:21 +0700

    Added pagination component to the apps

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import AddStudentForm from './components/AddStudentForm.jsx';
 import Selection from './components/EditSelection.jsx';
 import DeleteSeveralStudents from './components/DeleteSeveralStudents.jsx';
+import Pagination from './Pagination.jsx';
 import axios from "axios";
 
 function StudentRow({student, active, onActiveChange, stdID, onStdIDChange, handleDelete, handleEdit, editActive, onEditActiveChange, putName, setPutName, putStudentID, setPutStudentID, putAge, setPutAge, putSex, setPutSex, putAddress, setPutAddress, putSubject, setPutSubject}) {
@@ -69,7 +70,6 @@ function StudentTable({ children, filterText, active, onActiveChange }) {
 
     fetchPosts();
   }, [])
-  // console.log(studs);
 
   // Edit student list
   const handleEdit = async (e) => {
@@ -115,25 +115,6 @@ function StudentTable({ children, filterText, active, onActiveChange }) {
   const indexOfLastStudent = currentPage * studentsPerPage;
   const indexOfFirstStudent = indexOfLastStudent - studentsPerPage;
   const currentStudents = studs.slice(indexOfFirstStudent, indexOfLastStudent);
-
-  // Change page
-  const totalPages = Math.ceil(studs.length / studentsPerPage);
-  function handlePageUp() {             // a function to increment current page
-    if (currentPage >= totalPages){
-      setCurrentPage(1);
-    }
-    else {
-      setCurrentPage(currentPage + 1);
-    }
-  }
-  function handlePageDown() {           // a function to decrement current page
-    if (currentPage <= 1){
-      setCurrentPage(totalPages);
-    }
-    else {
-      setCurrentPage(currentPage - 1);
-    }
-  }
 
   const rows = [];
 
@@ -181,41 +162,13 @@ function StudentTable({ children, filterText, active, onActiveChange }) {
         </div>
       </div>
       {/* <!-- Pagination --> */}
-      <div className='flex items-center justify-end gap-x-1'>Showing<span> {currentStudents.length} </span>of<span> {studs.length} </span>records</div>
-      <nav className="flex items-center justify-end gap-x-1">
-        <div>
-          <span>Show</span>
-          <select value={studentsPerPage} onChange={(e) => setStudentsPerPage(e.target.value)}>
-            <option value='10'>10</option>
-            <option value='25'>25</option>
-            <option value='50'>50</option>
-            <option value='100'>100</option>
-          </select>
-          <span>per page</span>
-        </div>
-        <button onClick={handlePageDown} type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
-          <i className="fa-solid fa-angle-left fa-sm"></i>
-          <span aria-hidden="true" className="sr-only">Previous</span>
-        </button>
-        <div className="flex items-center gap-x-1">
-          <span className="min-h-[38px] min-w-[38px] flex justify-center items-center border border-gray-200 text-gray-800 py-2 px-3 text-sm rounded-lg focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:border-gray-700 dark:text-white dark:focus:bg-white/10">{currentPage}</span>
-          <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-gray-500">of</span>
-          <span className="min-h-[38px] flex justify-center items-center text-gray-500 py-2 px-1.5 text-sm dark:text-gray-500">{totalPages}</span>
-        </div>
-        <button onClick={handlePageUp} type="button" className="min-h-[38px] min-w-[38px] py-2 px-2.5 inline-flex justify-center items-center gap-x-2 text-sm rounded-lg text-gray-800 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10">
-          <span aria-hidden="true" className="sr-only">Next</span>
-          <i className="fa-solid fa-angle-right fa-sm"></i>
-        </button>
-      </nav>
+      <Pagination currentPage={currentPage} studentsPerPage={studentsPerPage} students={studs} setCurrentPage={setCurrentPage} setStudentsPerPage={setStudentsPerPage} currentStudents={currentStudents}/>
       {/* <!-- End Pagination --> */}
     </>
   );
 }
 
 function SearchBar({filterText, onFilterTextChange, active, onActiveChange}) {
-  // console.log(filterText);
-  // console.log(active);
-
   return (
     <div className='bg-[#435d7d] text-white min-w-full rounded-b-none rounded-t py-4 px-8'>
       <div className='flex flex-col items-center justify-between p-4 space-y-3 md:flex-row md:space-y-0 md:space-x-4'> {/* flex flex-row flex-wrap items-stretch justify-between content-stretch */}
